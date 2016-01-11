b073383996fd59bcee6cf9cb7b0ae0ec365cdad7
Patrice Lebel <patrice.lebel@outlook.com>
2016-01-11 01:10:06 -0500

    it turns out that locale_helper.rb is needed

--- lib/solidus_i18n/configuration.rb
+++ lib/solidus_i18n/configuration.rb
@@ -1,4 +1,9 @@
 module SolidusI18n
   class Configuration < Spree::Preferences::Configuration
+    # These configs intend to, respectively:
+    #
+    #   Set locales that should be available for end users (not used anymore excepts for helpers used by solidus_globalize)
+    #
+    preference :available_locales, :array, default: [:en]
   end
 end
